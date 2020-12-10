a48435da20720116a30f0c9b22a51bf5da029bd7
DanniloCN <dannilocarlos@gmail.com>
2020-12-10 14:30:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-pnt-8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-pnt-8)
---
+++ pages/home/home.ts
@@ -0,0 +1,49 @@
+import { Component } from "@angular/core";
+import { NavController, AlertController, ToastController } from "ionic-angular";
+
+@Component({
+  selector: "page-home",
+  templateUrl: "home.html",
+  styleUrls: ["home.scss"]
+})
+export class HomePage {
+  constructor(public navCtrl: AlertController, public alertCtrl: AlertController, public toastCtrl: ToastController) {
+
+ }
+
+  abrirAlerta() {
+    let alert = this.alertCtrl.create({
+      title: "Confirmação de compra",
+      message: "Tem certeza que deseja comprar esse livro?",
+      buttons: [
+                {
+          text: "Cancelar",
+          handler: () => {this.presentToastCancela()}
+        },
+        {
+          text: "Comprar",
+          handler: () => {this.presentToastSucesso()}
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  presentToastSucesso() {
+    let toast = this.toastCtrl.create({
+      message: "Sua compra foi realizada com sucesso!",
+      duration: 3000,
+      position: "bottom"
+    });
+    toast.present();
+  }
+  presentToastCancela() {
+    let toast = this.toastCtrl.create({
+      message: "Sua compra foi cancelada!",
+      duration: 3000,
+      position: "bottom"
+    });
+    toast.present();
+  }
+
+}
